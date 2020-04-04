0a557ed9ac89f86c32d55c2d7a4d4f36fd057ed8
Samvel <41449047+sttie@users.noreply.github.com>
2020-04-04 19:03:05 +0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,11 +1,15 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QMainWindow,
-                                QMenu, QAction)
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QGridLayout,
+                                QMenu, QAction, QLineEdit, QLabel, QPushButton)
 from PyQt5.QtCore import QSize
+import handlers
+from functools import partial
 
 class CalcWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.active_widgets = []
+        self.answer_field = QLabel(self)
         self.initWindow()
     
     def initWindow(self):
@@ -14,15 +18,20 @@ class CalcWindow(QMainWindow):
         natural = menubar.addMenu("Натуральные числа")
         
         n_sum = QAction("Сумма", self)
-        n_sum.triggered.connect(self.sum_window)
-        n_dif = QAction("Разность", self)
+        n_sum.triggered.connect(self.add_window)
+
+        n_sub = QAction("Разность", self)
+        n_sub.triggered.connect(self.sub_window)
+
         n_mult = QAction("Умножение", self)
+        n_mult.triggered.connect(self.mult_window)
+
         n_div = QAction("Деление", self)
         n_mod = QAction("Деление с остатком", self)
         n_GCD = QAction("НОД", self)
         n_LCM = QAction("НОК", self)
         
-        self.setActions(natural, [n_sum, n_dif, n_mult, n_div,
+        self.setBar(natural, [n_sum, n_sub, n_mult, n_div,
                                   n_mod, n_GCD, n_LCM])
 
 
@@ -38,26 +47,73 @@ class CalcWindow(QMainWindow):
         w_LCM = QAction("НОК", self)
 
         # НАДО ЧТО-ТО ДОБАВИТЬ
-        self.setActions(whole, [w_abs, w_sum, w_dif, w_mult,
+        self.setBar(whole, [w_abs, w_sum, w_dif, w_mult,
                                   w_div, w_mod, w_GCD, w_LCM])
 
 
         rational = menubar.addMenu("Рациональные числа")
         polynoms = menubar.addMenu("Многочлены")
 
-        self.setFixedSize(QSize(650, 550))
+        self.setFixedSize(QSize(550, 350))
         self.move(300, 300)
         self.setWindowTitle("Компьютерная алгебра")
         
         self.show()
 
+    
+    # Заполняем окно для "Натурльные числа" по шаблону
+    def fillNaturalsWindow(self, buttonName, actionFunc):
+        self.clearWidgets()
 
-    def setActions(self, bar, actions):
-        for action in actions:
-            bar.addAction(action)
+        self.firstNumLine = QLineEdit(self)
+        self.secondNumLine = QLineEdit(self)
+        self.actionButton = QPushButton(self)
+        self.active_widgets = [self.firstNumLine, self.secondNumLine, self.actionButton]
 
-    def sum_window(self):
+        self.firstNumLine.move(20, 40)
+        self.secondNumLine.move(140, 40)
+        self.actionButton.move(260, 40)
+        self.actionButton.resize(180, 30)
+        self.actionButton.setText(buttonName)
+        self.actionButton.clicked.connect(partial(actionFunc, self))
+
+        self.answer_field.move(41, 80)
+        # Большая ширина, чтобы влезали ответы
+        self.answer_field.resize(400, 30)
+
+        self.showWidgets()
+
+
+    def add_window(self):
+        self.fillNaturalsWindow("Вычислить сумму", handlers.naturals_sum)
+        
+
+    def sub_window(self):
+        self.fillNaturalsWindow("Вычислить разность", handlers.naturals_sub)
+
+
+    def mult_window(self):
+        self.fillNaturalsWindow("Вычислить произведение", handlers.naturals_mult)
+
+
+    def setBar(self, bar, objs):
+        for obj in objs:
+            bar.addAction(obj)
+
+    
+    def setActions(self, actions):
         pass
+
+
+    def showWidgets(self):
+        for widget in self.active_widgets:
+            widget.show()
+
+
+    def clearWidgets(self):
+        for widget in self.active_widgets:
+            del widget
+
 
 
 if __name__ == "__main__":
