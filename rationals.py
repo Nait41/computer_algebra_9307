51bdab20eb9e89499ea5c3120bb0366113b58900
Samvel <41449047+sttie@users.noreply.github.com>
2020-04-09 00:56:42 +0300

    Update rationals.py

--- rationals.py
+++ rationals.py
@@ -1,18 +1,33 @@
 from whole import Whole, TRANS_N_Z
 
 class Rational:
-    # По умолчанию знаменатель = 1
     def __init__(self, numerator, denominator=Whole("1")):
         self.numerator = numerator
         self.denominator = denominator
     
     """
-    Автор: Арутюнян Самвел
-    Назначение: преобразование из дробного в целое
+    Автор: Торопов Валентин
+    Назначение: сокращение дроби
     """
-    def TRANS_Q_Z(self):
-        # ПРОВЕРЯТЬ, РАВЕН ЛИ ЗНАМЕНАТЕЛЬ 1 ПЕРЕД ВЫЗОВОМ
-        return self.numerator
+    def RED_Q_Q(self):
+        temp_numerator = self.numerator.ABS_Z_N()
+        temp_denominator = self.denominator.ABS_Z_N()
+
+        gcd = temp_numerator.GCF_NN_N(temp_denominator)
+
+        temp_numerator //= gcd
+        temp_denominator //= gcd
+        new_rational = Rational(temp_numerator, temp_denominator)
+
+        return new_rational
+
+
+    """
+    Автор: Торопов Валентин
+    Назначение: проверка на целое
+    """
+    def INT_Q_B(self):
+        return 1 if self.denominator == Whole("1") else 0
 
 
     """
@@ -46,7 +61,7 @@ class Rational:
 
     """
     Автор: Арутюнян Самвел
-    Назначение: деление дробей
+    Назначение: умножение дробей
     Каноническое название: MUL_QQ_Q
     """
     def __mult__(self, other):
@@ -57,9 +72,48 @@ class Rational:
         return new_rational
 
     
+    """
+    Автор: 
+    Назначение: деление дробей
+    Каноническое название: DIV_QQ_Q
+    """
+    def __truediv__(self, other):
+        new_numerator = self.numerator * other.denominator
+        new_denominator = self.denominator * other.numerator
+        new_rational = Rational(new_numerator, new_denominator)
+
+        return new_rational
+
+    """
+    Автор: Арутюнян Самвел
+    Назначение: деление дробей
+    Каноническое название: DIV_QQ_Q
+    """
+    def __floordiv__(self, other):
+        return self / other
+
+    
     def __neg__(self):
         return Rational(self.numerator.MUL_ZM_Z(), self.denominator)
 
     
     def __str__(self):
         return "{}/{}".format(self.numerator, self.denominator)
+
+
+"""
+Автор: Торопов Валентин
+Назначение: преобразование из целого в дробное
+"""
+def TRANS_Z_Q(whole_number):
+    new_rational = Rational(whole_number, Whole("1"))
+    return new_rational
+
+
+"""
+Автор: Арутюнян Самвел
+Назначение: преобразование из дробного в целое
+"""
+def TRANS_Q_Z(rational_number):
+    # ПРОВЕРЯТЬ, РАВЕН ЛИ ЗНАМЕНАТЕЛЬ 1 ПЕРЕД ВЫЗОВОМ
+    return rational_number.numerator
