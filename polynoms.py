48814eeedc38af00d7e82633bc44f123feaa5d38
Nait41 <63399368+Nait41@users.noreply.github.com>
2020-04-09 13:43:40 +0300

    Update polynoms.py

--- polynoms.py
+++ polynoms.py
@@ -1,28 +1,68 @@
 class Polynom:
     def __init__(self, coefs_dict={}):
         self.coefs_dict = coefs_dict
-
+    """
+    Автор: Артем
+    Назначение: умножение многочлена на x^k
+    Каноническое название: MUL_Pxk_P
+    """
+    def MUL_Pxk_P(polynom1, num):
+    polynom2=dict()
+    for key in polynom1:
+        polynom2[key]=ADD_QQ_Q(polynom1[key], num)
+    return polynom2
 
     """
-    Автор:
+    Автор: Артем
+    Назначение: умножение многочлена на рациональное число
+    Каноническое название: MUL_PQ_P
+    """
+    def MUL_PQ_P(polynom1, num):
+    polynom2=dict()
+    for key in polynom1:
+        polynom2[key]=MULL_QQ_Q(polynom1[key], num)
+    return polynom2
+
+    """
+    Автор: Артем
     Назначение: сложение многочленов
     Каноническое название: ADD_PP_P
     """
-    def __add__(self, other):
-        pass
-
+    def __add__(polynom1, polynom2):
+        polynom3=dict()
+        if len(polynom1)>len(polynom2):
+            for key in polynom1:
+                polynom3[key]=polynom1[key]
+                if key in polynom2:
+                    polynom3[key]=ADD_QQ_Q(polynom3[key],polynom2[key])
+        else:
+            for key in polynom2:
+                polynom3[key]=polynom2[key]
+                if key in polynom1:
+                    polynom3[key]=ADD_QQ_Q(polynom3[key],polynom1[key])
+        return polynom3
 
     """
-    Автор:
+    Автор: Артем
     Назначение: вычитание многочленов
     Каноническое название: SUB_PP_P
     """
-    def __sub__(self, other):
-        pass
-
+    def __sub__(polynom1, polynom2):
+        polynom3=dict()
+        if len(polynom1)>len(polynom2):
+            for key in polynom1:
+                polynom3[key]=polynom1[key]
+                if key in polynom2:
+                    polynom3[key]=SUB_QQ_Q(polynom3[key],polynom2[key])
+        else:
+            for key in polynom2:
+                polynom3[key]=polynom2[key]
+                if key in polynom1:
+                    polynom3[key]=SUB_QQ_Q(polynom3[key],polynom1[key])
+        return polynom3
 
     """
-    Автор:
+    Автор: 
     Назначение: умножение многочленов
     Каноническое название: MUL_PP_P
     """
